fb1ecfba0cd33dc457347eb574ec15a734236909
cqchy <2136404469@qq.com>
2024-05-25 18:30:43 +0800

    Add files via upload

---
+++ music/README.md
@@ -0,0 +1,17 @@
+FlyMusic静态网页播放器
+
+官方网站[https://flymusic.hkncloud.cn]
+
+✨特征:
+
+歌单形式，无需手动添加歌曲
+
+可自定义添加任意个歌单，左右切换
+
+支持的音乐平台:netease, tencent
+
+食用方法:
+1. Fork一份
+2. 修改index.html文件
+3. 修改域名
+
---
+++ music/js/main.js
@@ -0,0 +1,17 @@
+var container = document.getElementById("main");
+
+// 创建一个HTML字符串
+var htmlContent = '<div class="swiper-container"><div class="swiper-button-prev"></div><div class="swiper-button-next"></div><div class="swiper-wrapper"><div class="swiper-slide"><div class="aplayer-wrap"><h3 style="text-align: center">✨云音乐热歌榜✨</h3><meting-js list-max-height="330px"server="netease"type="playlist"id="3778678"></meting-js></div></div><div class="swiper-slide"><div class="aplayer-wrap"><h3 style="text-align: center">✨云音乐飙升榜✨</h3><meting-js preload="none"list-max-height="330px"server="netease"type="playlist"id="19723756"></meting-js></div></div><div class="swiper-slide"><div class="aplayer-wrap"><h3 style="text-align: center">✨云音乐新歌榜✨</h3><meting-js preload="none"list-max-height="330px"server="netease"type="playlist"id="3779629"></meting-js></div></div><div class="swiper-slide"><div class="aplayer-wrap"><h3 style="text-align: center">✨推荐云歌单✨</h3><meting-js preload="none"list-max-height="330px"server="netease"type="playlist"id="642330848"></meting-js></div></div><div class="swiper-slide"><div class="aplayer-wrap"><h1>FlyMusic项目介绍</h1><style>body{font-family:Arial,sans-serif;margin:20px}h1,h2{text-align:center}.details{margin-bottom:20px}.details dt,.details dd{display:inline-block;margin:0;vertical-align:top;margin-right:10px}.details dt{font-weight:bold}</style><div class="details"><dl><dt>开发者：</dt><dd>cqchy</dd><br><dt>归属公司：</dt><dd>重庆炫讯云信息技术有限公司</dd><br><dt>开发完成日期：</dt><dd>2024年5月25日</dd></dl></div><p>本程序现已经开源！您可以在GitHub上进行下载、分发、修改！</p><p>本程序为开源产品，开源版不收取任何费用！</p><p>您可以购买专业版程序，包括我们的技术指导以及程序更新！</p><p>开源版不支持</p></div></div></div><div class="landing-buttons"><a class="landing-button"target="_blank"href="https://github.com/cqchy/flymusic/"style="text-decoration:none">开源仓库</a><a class="landing-button"target="_blank"href="https://docs.qq.com/doc/DS3ZBYXJUZ2lSYWha"style="text-decoration:none">官方文档</a><a class="landing-button"target="_blank"href="https://flymusic.hkncloud.cn/buy"style="text-decoration:none">专业版购买</a></div>';
+
+// 将HTML内容设置为container元素的innerHTML
+container.innerHTML = htmlContent;
+
+
+var container = document.getElementById("footer");
+
+// 创建一个HTML字符串
+var htmlContent = '<footer class="footer"><p>Copyright©2024 FlyMusic by<a href="https://hkncloud.cn"style="text-decoration:none">重庆炫讯云信息技术有限公司</a></p></footer>';
+
+// 将HTML内容设置为container元素的innerHTML
+container.innerHTML = htmlContent;
+
